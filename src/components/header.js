9abfb1757037654a43570cfe726e10b4a2ec93da
Shuting <shutingzeng.damu@gmail.com>
2019-03-17 17:15:41 -0500

    My React App

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,47 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from '../images/logo.svg'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+
+class Header extends React.Component {
+  constructor(props) {
+  super(props)
+
+this.state = {
+  hasScrolled: false
+    }
+}
+
+  componentDidMount() {
+    window.addEventListener('scroll',this.handleScroll)
+  }
+
+handleScroll = (event) => {
+  const scrollTop = window.pageYOffset
+
+  if (scrollTop >50) {
+    this.setState({hasScrolled: true})
+  } else {
+    this.setState({hasScrolled: false})
+  }
+}
+
+
+ render() {
+  return (
+   <div className={this.state.hasScrolled ?'Header HeaderScrolled' : 'Header'}>
+   <div className="HeaderGroup">
+    <Link to="/"><img src={logo} width="50" /></Link>
+    <Link to="/atoms">Atoms</Link>
+    <Link to="/molecules">Molecules</Link>
+    <Link to="/organisms">Organisms</Link>
+    <Link to="/templates">Templates</Link>
+    <Link to="/pages"><button>Pages</button></Link>
   </div>
-)
+  </div>
+    )
+  }
+ }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,58 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
+import Section from '../components/Section';
+import Wave from '../components/Wave';
 
 const IndexPage = () => (
   <div>
-    <h1>Design System Demo - Built with React</h1>
-    <p>This demo uses nodejs, gastby, visual code editor, etc., to present how to connect visual components to the website directly.</p>
-    <p>Check it out!</p>
-    <Link to="/page-2/">Go to my demo</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+       <h1> Design System  Built with  <br /> React</h1>
+       <p>This demo uses React, nodejs, Gastby, visual code editor, etc., to present how to connect visual components to the website directly.</p>
+       <Link to="/page-2/">Go to my demo</Link>
+       <div className="Logos">
+        <img src={require('../images/atomic-design.svg')} width="500"></img>
+       </div>
+    <Wave />  
+   </div>
+   </div>
+   <div className="Cards">
+      <h2>From Parts to Whole</h2>
+      <div className="CardGroup">
+      <Card
+      title="colors"
+      text="3 palettes"
+      image={require('../images/card.jpg')}/>
+      <Card
+      title="colors"
+      text="3 palettes"
+      image={require('../images/card.jpg')}/>
+      <Card
+      title="colors"
+      text="3 palettes"
+      image={require('../images/card.jpg')}/>
+      <Card
+      title="colors"
+      text="3 palettes"
+      image={require('../images/card.jpg')}/>
+      <Card
+      title="colors"
+      text="3 palettes"
+      image={require('../images/card.jpg')} />
+      </div>
   </div>
+   <Section
+    image={require('../images/section.jpg')}
+    title="Shuting Zeng"
+    text="I use design, storytelling, and multi-disciplinary thinking to align user needs with product development and business goals. "
+    />
+</div>
 )
 
 export default IndexPage
+
+
+
+
+
